dcbe84287b5b7580c0f65c08450c1d606e2db55e
Alice de Bokay <alicedebokay@googlemail.com>
2020-11-24 14:51:23 +0100

    start TaxiFareModel package

---
+++ TaxiFareModel/encoders.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from TaxiFareModel.data import haversine_vectorized
+
+class TimeFeaturesEncoder(BaseEstimator, TransformerMixin):
+    """Extract the day of week (dow), the hour, the month and the year from a
+    time column."""
+    def __init__(self, time_column, time_zone_name='America/New_York'):
+        self.time_column = time_column
+        self.time_zone_name = time_zone_name
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        """Returns a copy of the DataFrame X with only four columns: 'dow', 'hour', 'month', 'year'"""
+        X_ = X.copy()
+        time_feat = extract_time_features(X_)[['dow', 'hour', 'month', 'year']]
+        return time_feat
+
+class DistanceTransformer(BaseEstimator, TransformerMixin):
+    """Compute the haversine distance between two GPS points."""
+    def __init__(self,
+                 start_lat="pickup_latitude",
+                 start_lon="pickup_longitude",
+                 end_lat="dropoff_latitude",
+                 end_lon="dropoff_longitude"):
+        self.start_lat = start_lat
+        self.start_lon = start_lon
+        self.end_lat = end_lat
+        self.end_lon = end_lon
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        """Returns a copy of the DataFrame X with only one column: 'distance'"""
+        X_ = X.copy()
+        X_["distance"] = haversine_vectorized(X_)
+        distance = pd.DataFrame(X_["distance"])
+        return distance
---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,70 @@
+# imports
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+from TaxiFareModel.data import get_data, clean_data
+from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
+from TaxiFareModel.utils import compute_rmse
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+        distance_pipe = make_pipeline(DistanceTransformer(), StandardScaler())
+
+        time_pipe = make_pipeline(
+            TimeFeaturesEncoder(time_column = 'pickup_datetime'),
+            OneHotEncoder(handle_unknown = 'ignore')
+            )
+
+        preprocessor = ColumnTransformer([
+            ('distance_trans', distance_pipe, ['pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude']),
+            ('time_trans', time_pipe, ['pickup_datetime'])])
+
+        model_pipeline = Pipeline(steps = [('preprocessing', preprocessor),
+                                            ('regressor', LinearRegression())])
+
+        self.pipeline = model_pipeline
+
+    def run(self):
+        """set and train the pipeline"""
+        self.pipeline.fit(self.X, self.y)
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        y_pred = self.pipeline.predict(X_test)
+        return compute_rmse(y_pred, y_test)
+
+
+if __name__ == "__main__":
+    # get & clean data
+    data = clean_data(get_data())
+
+    # set X and y
+    X = data.drop(columns = ['fare_amount'])
+    y = data['fare_amount']
+
+    # hold out
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+    # build pipeline
+    pipeline = Trainer().set_pipeline()
+
+    # train the pipeline
+    model = pipeline.run(X_train, y_train, pipeline)
+
+    # evaluate the pipeline
+    result = pipeline.evaluate(X_test, y_test, model)
+
+    print(result)
